a3a8ae1eddb0c3e5c1437421d1fff74509d559a9
mohammedrimshan <rimshanshanu55@gmail.com>
2024-12-28 10:13:23 +0530

    Changes made

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,96 +1,96 @@
-// import { io } from "socket.io-client";
-// import Cookies from "js-cookie";
-// import axiosInterceptor from "@/axiosInstance";
-// const SOCKET_URL = import.meta.env.VITE_BASE_URL || "https://edusphere-backend.rimshan.in:5000";
+import { io } from "socket.io-client";
+import Cookies from "js-cookie";
+import axiosInterceptor from "@/axiosInstance";
+const SOCKET_URL = "https://edusphere-backend.rimshan.in";
 
-// const getAuthDetails = () => {
-//   const tutorToken = Cookies.get("tutor_access_token");
-//   const studentToken = Cookies.get("student_access_token");
+const getAuthDetails = () => {
+  const tutorToken = Cookies.get("tutor_access_token");
+  const studentToken = Cookies.get("student_access_token");
 
-//   if (tutorToken) {
-//     return { role: "tutor", token: tutorToken };
-//   } else if (studentToken) {
-//     return { role: "student", token: studentToken };
-//   }
-//   return { role: null, token: null };
-// };
+  if (tutorToken) {
+    return { role: "tutor", token: tutorToken };
+  } else if (studentToken) {
+    return { role: "student", token: studentToken };
+  }
+  return { role: null, token: null };
+};
 
-// let { role, token } = getAuthDetails();
+let { role, token } = getAuthDetails();
 
-// let isRefreshing = false;
+let isRefreshing = false;
 
-// const socket = io(SOCKET_URL, {
-//   auth: {
-//     token: token || null,
-//     role,
-//   },
-//   transports: ["websocket", "polling"],
-//   reconnection: true,
-//   reconnectionAttempts: 5,
-//   reconnectionDelay: 1000,
-// });
+const socket = io(SOCKET_URL, {
+  auth: {
+    token: token || null,
+    role,
+  },
+  transports: ["websocket", "polling"],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
 
-// socket.on("connect", () => {
-//   console.log("Socket connected successfully");
-//   isRefreshing = false;
-// });
+socket.on("connect", () => {
+  console.log("Socket connected successfully");
+  isRefreshing = false;
+});
 
-// socket.on("connect_error", (error) => {
-//   console.error("Socket connection error:", error.message);
-//   // if (error.message === "Invalid token. Please login again.") {
-//   //   Cookies.remove("tutor_access_token");
-//   //   Cookies.remove("tutorRefreshToken");
-//   //   Cookies.remove("studentRefreshToken");
-//   //   Cookies.remove("student_access_token");
-//   //   window.location.href =
-//   //     role === "tutor" ? "/tutor/signin" : "/student/signin";
-//   // }
-//   console.log("Connection error", error)
-// });
+socket.on("connect_error", (error) => {
+  console.error("Socket connection error:", error.message);
+  // if (error.message === "Invalid token. Please login again.") {
+  //   Cookies.remove("tutor_access_token");
+  //   Cookies.remove("tutorRefreshToken");
+  //   Cookies.remove("studentRefreshToken");
+  //   Cookies.remove("student_access_token");
+  //   window.location.href =
+  //     role === "tutor" ? "/tutor/signin" : "/student/signin";
+  // }
+  console.log("Connection error", error)
+});
 
-// socket.on("token-expired", async () => {
-//   if (isRefreshing) {
-//     console.log("Refresh already in progress. Skipping additional attempts.");
-//     return;
-//   }
+socket.on("token-expired", async () => {
+  if (isRefreshing) {
+    console.log("Refresh already in progress. Skipping additional attempts.");
+    return;
+  }
 
-//   isRefreshing = true; 
-//   console.log("Token expired, attempting to refresh...");
+  isRefreshing = true; 
+  console.log("Token expired, attempting to refresh...");
 
-//   try {
-//     const refreshResponse = await axiosInterceptor.post(
-//       "/auth/refresh",
-//       {},
-//       { withCredentials: true }
-//     );
+  try {
+    const refreshResponse = await axiosInterceptor.post(
+      "/auth/refresh",
+      {},
+      { withCredentials: true }
+    );
 
-//     const { access_token, role: refreshedRole } = refreshResponse.data;
+    const { access_token, role: refreshedRole } = refreshResponse.data;
 
-//     Cookies.set(`${refreshedRole}_access_token`, access_token, {
-//       expires: 13 / (24 * 60),
-//     });
+    Cookies.set(`${refreshedRole}_access_token`, access_token, {
+      expires: 13 / (24 * 60),
+    });
 
-//     socket.auth.token = access_token;
-//     socket.auth.role = refreshedRole;
+    socket.auth.token = access_token;
+    socket.auth.role = refreshedRole;
 
-//     socket.connect();
-//     console.log("Token refreshed and socket reconnected successfully.");
-//   } catch (error) {
-//     console.error("Failed to refresh token (in Socket): ", error);
+    socket.connect();
+    console.log("Token refreshed and socket reconnected successfully.");
+  } catch (error) {
+    console.error("Failed to refresh token (in Socket): ", error);
 
-//     // Cookies.remove("tutor_access_token");
-//     // Cookies.remove("student_access_token");
+    // Cookies.remove("tutor_access_token");
+    // Cookies.remove("student_access_token");
 
-//     // window.location.href =
-//     //   role === "tutor" ? "/tutor/signin" : "/student/signin";
-//     console.log("REFRESHING ERRORR", error)
-//     } finally {
-//     isRefreshing = false;
-//   }
-// });
+    // window.location.href =
+    //   role === "tutor" ? "/tutor/signin" : "/student/signin";
+    console.log("REFRESHING ERRORR", error)
+    } finally {
+    isRefreshing = false;
+  }
+});
 
-// socket.on("disconnect", (reason) => {
-//   console.log("Socket disconnected:", reason);
-// });
+socket.on("disconnect", (reason) => {
+  console.log("Socket disconnected:", reason);
+});
 
-// export default socket;
+export default socket;
